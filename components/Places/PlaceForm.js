c0a4b763d15d3cc4c4bd7a27c9739cefe889bc4a
Nikita Kravchenko <nickravchenko98@gmail.com>
2023-04-22 19:26:24 -0400

    Start working on screens

---
+++ components/Places/PlaceForm.js
@@ -0,0 +1,13 @@
+import { View, Text, ScrollView } from "react-native";
+
+function PlaceForm() {
+  return (
+    <ScrollView>
+      <View>
+        <Text>Title</Text>
+      </View>
+    </ScrollView>
+  );
+}
+
+export default PlaceForm;
