6edeffa109d3cfe5d5330cc1bfaf7fc2f510545c
Nikita Kravchenko <nickravchenko98@gmail.com>
2023-04-27 12:08:16 -0400

    Picture taker

--- components/Places/PlaceForm.js
+++ components/Places/PlaceForm.js
@@ -1,13 +1,49 @@
-import { View, Text, ScrollView } from "react-native";
+import { useState } from "react";
+import { View, Text, ScrollView, TextInput, StyleSheet } from "react-native";
+import { Colors } from "./../../constants/colors";
+import ImagePicker from "./ImagePicker";
 
 function PlaceForm() {
+  const [enteredTitle, setEnteredTitle] = useState("");
+
+  function changeTitleHandler(enteredText) {
+    setEnteredTitle(enteredText);
+  }
   return (
-    <ScrollView>
+    <ScrollView style={styles.form}>
       <View>
-        <Text>Title</Text>
+        <Text style={styles.label}>Title</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={changeTitleHandler}
+          value={enteredTitle}
+        />
       </View>
+      <ImagePicker />
     </ScrollView>
   );
 }
 
 export default PlaceForm;
+
+const styles = StyleSheet.create({
+  form: {
+    flex: 1,
+    padding: 24,
+  },
+  label: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 4,
+    color: Colors.primary500,
+  },
+  input: {
+    marginVertical: 8,
+    marginHorizontal: 4,
+    paddingVertical: 8,
+    fontSize: 16,
+    borderBottomColor: Colors.primary700,
+    borderBottomWidth: 2,
+    backgroundColor: Colors.primary100,
+  },
+});
